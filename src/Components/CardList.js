bc3d0c0a6d3720f929e61210a275f19c18b52597
adammuana20 <131564307+adammuana20@users.noreply.github.com>
2023-09-02 08:31:17 +0800

    Update CardList.js

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -2,14 +2,8 @@ import React from "react";
 import Card from './Card'
 
 export default function CardList({ robots }){
-    const cardItems = robots.map((user, i) => {
-        return (
-            <Card 
-                key={i}
-                id={robots[i].id}
-                name={robots[i].name}
-                email={robots[i].email}
-            />
+    const cardItems = robots.map((robot) => {
+        return <Card key={robot.id} robot={robot}/>
         )
     })
     return (
